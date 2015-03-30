c4bf97d51424cacb30c9acb4407d7c10141682b3
Brian Toba <toba.brian@gmail.com>
2015-03-29 21:55:59 -0700

    Adding move handling for touch events

--- www/flyfly.js
+++ www/flyfly.js
@@ -1,23 +1,72 @@
-var Ship = function(x, y) {
+var Ship = function(x, y, canvas) {
     this.x = x;
     this.y = y;
     this.dead = false;
+    this.moving = false;
 
     this.img = new Image();
     this.img.src = "/img/ship.png";
 
     this.Draw = function(context) {
-        context.drawImage(this.img, this.x, this.y, 40, 40);
+        var size = 150;
+        context.drawImage(this.img, this.x - size / 2, this.y - size / 2, size, size);
     }
+
+    var that = this;
+
+    var status = document.getElementById("status");
+
+    this.handleStart = function() {
+        that.moving = true;
+    }
+
+    this.handleEnd = function() {
+        that.moving = false;
+        status.innerText = "Not moving";
+    }
+
+    this.handleMove = function(e) {
+        e.preventDefault();
+
+        var speed = 5;
+        if (that.moving) {
+            var newX = that.x - e.changedTouches[0].pageX,
+                newY = that.y - e.changedTouches[0].pageY;
+
+            that.x -= newX < 0 ? Math.max(newX, -speed) : Math.min(newX, speed);
+            that.y -= newY < 0 ? Math.max(newY, -speed) : Math.min(newY, speed);
+
+            status.innerText = "Moving to: " + that.x + ", " + that.y;
+        }
+    }
+
+    canvas.addEventListener("touchstart", this.handleStart, false);
+    canvas.addEventListener("touchend", this.handleEnd, false);
+    canvas.addEventListener("touchmove", this.handleMove, false);
+    canvas.addEventListener("mousedown", this.handleStart, false);
+    canvas.addEventListener("mouseup", this.handleEnd, false);
+    canvas.addEventListener("mousemove", this.handleMove, false);
 };
 
+var mainCanvas = document.getElementById('mainCanvas');
+
 function Render(ship) {
-    var context = document.getElementById('mainCanvas').getContext("2d");
+    var context = mainCanvas.getContext("2d");
+
+    // Clear canvas
+    context.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
+
     ship.Draw(context);
 }
 
-var ship = new Ship(10, 10);
+// Game
+
+var ship = new Ship(10, 10, mainCanvas);
+
+// Game loop
+
+var fps = 60;
 setInterval(function() {
     Render(ship);
-}, 200);
+}, 1000 / fps);
 
