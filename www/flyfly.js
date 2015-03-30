13484580c7861292a9e5274213d27eee3eec5add
Brian Toba <toba.brian@gmail.com>
2015-03-29 22:18:09 -0700

    Changing move code to use intervals

--- www/flyfly.js
+++ www/flyfly.js
@@ -1,13 +1,18 @@
+var FPS = 60;
+
 var Ship = function(x, y, canvas) {
     this.x = x;
     this.y = y;
     this.dead = false;
     this.moving = false;
+    this.movingIntervalId = -1;
+    this.dirX = 0;
+    this.dirY = 0;
 
     this.img = new Image();
     this.img.src = "/img/ship.png";
 
-    this.Draw = function(context) {
+    this.draw = function(context) {
         var size = 150;
         context.drawImage(this.img, this.x - size / 2, this.y - size / 2, size, size);
     }
@@ -16,31 +21,45 @@ var Ship = function(x, y, canvas) {
 
     var status = document.getElementById("status");
 
-    this.handleStart = function() {
-        that.moving = true;
-    }
-
-    this.handleEnd = function() {
-        that.moving = false;
-        status.innerText = "Not moving";
-    }
-
-    this.handleMove = function(e) {
-        e.preventDefault();
-
-        var speed = 5;
+    this.doMove = function () {
+        var speed = 20;
         if (that.moving) {
-            var initialX = e.clientX || e.changedTouches[0].pageX;
-            var initialY = e.clientY || e.changedTouches[0].pageY;
-
-            var newX = that.x - initialX,
-                newY = that.y - initialY;
+            var newX = that.x - that.dirX,
+                newY = that.y - that.dirY;
 
             that.x -= newX < 0 ? Math.max(newX, -speed) : Math.min(newX, speed);
             that.y -= newY < 0 ? Math.max(newY, -speed) : Math.min(newY, speed);
 
             status.innerText = "Moving to: " + that.x + ", " + that.y;
         }
+    }
+
+    this.handleStart = function(e) {
+        that.moving = true;
+
+        if (that.movingIntervalId == -1) {
+            that.movingIntervalId = setInterval(function() {
+                that.doMove();
+            }, 1000 / FPS);
+        }
+    }
+
+    this.handleEnd = function() {
+        that.moving = false;
+        status.innerText = "Not moving";
+
+        // Stop following the finger
+        if (that.movingIntervalId != -1) {
+            clearInterval(that.movingIntervalId);
+            that.movingIntervalId = -1;
+        }
+    }
+
+    this.handleMove = function(e) {
+        e.preventDefault();
+
+        that.dirX = e.clientX || e.changedTouches[0].pageX;
+        that.dirY = e.clientY || e.changedTouches[0].pageY;
     }
 
     canvas.addEventListener("touchstart", this.handleStart, false);
@@ -52,6 +71,8 @@ var Ship = function(x, y, canvas) {
 };
 
 var mainCanvas = document.getElementById('mainCanvas');
+mainCanvas.width = document.body.clientWidth;
+mainCanvas.height = document.body.clientHeight;
 
 function Render(ship) {
     var context = mainCanvas.getContext("2d");
@@ -59,7 +80,7 @@ function Render(ship) {
     // Clear canvas
     context.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
 
-    ship.Draw(context);
+    ship.draw(context);
 }
 
 // Game
@@ -68,8 +89,7 @@ var ship = new Ship(10, 10, mainCanvas);
 
 // Game loop
 
-var fps = 60;
 setInterval(function() {
     Render(ship);
-}, 1000 / fps);
+}, 1000 / FPS);
 
